c38337bab0ff2aa00011b9c1f5b997aec58f6a7d
Piyushnmj <piyushnmj@yahoo.com>
2022-11-20 17:21:15 +0530

    Insertion Sort

--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -4,8 +4,19 @@
     {
         static void Main(string[] args)
         {
-            BinarySearchWord obj= new BinarySearchWord();
-            obj.WordExistsOrNot();
+            Console.WriteLine("Welcome to Algorithm Programs");
+
+            string[] names = { "OOPS","Array","Dictionary","Generics","Delegates","LinkedList","Stack","Queue","HashTable","Events","Regular Expressions" };
+            InsertionSortMethod objInsertion = new InsertionSortMethod();
+
+            Console.WriteLine("\nBefore sorting array elements are: ");
+            objInsertion.Display(names);
+
+            objInsertion.Sort(names);
+            Console.WriteLine("\n\nAfter sorting array elements are: ");
+            objInsertion.Display(names);
+
+            Console.ReadLine();
         }
     }
 }
