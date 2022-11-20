05e88c078b04cd0582a19f7744f0b247b8bbe3e2
Piyushnmj <piyushnmj@yahoo.com>
2022-11-20 16:20:00 +0530

    Binary Search the word from word list

--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -4,20 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Algorithm Programs");
-
-            Console.WriteLine("Enter the string : ");
-            string word = Console.ReadLine();
-            int n = word.Length;
-            string answer = " ";
-
-            Console.WriteLine("\nAll possible strings are : ");
-
-            Console.WriteLine("\nIterative Method: ");
-            PermutationOfString.Iterative(word, answer);
-
-            Console.WriteLine("\nRecursive Method: ");
-            PermutationOfString.Recursion(word, 0, n-1);
+            BinarySearchWord obj= new BinarySearchWord();
+            obj.WordExistsOrNot();
         }
     }
 }
