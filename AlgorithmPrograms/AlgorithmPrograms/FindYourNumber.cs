763b874ddb2b1825af68583e48ea39d5639092c6
Piyushnmj <piyushnmj@yahoo.com>
2022-11-25 13:45:05 +0530

    Find Your Number Game

---
+++ AlgorithmPrograms/AlgorithmPrograms/FindYourNumber.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class FindYourNumber
+    {
+        public void FindNumber()
+        {
+            int low = 0;
+            int high = 100;
+            int count = 0;
+
+            Console.WriteLine("\nLets Play Guess the Number game \nThink of a number between 0 and 100. \nPress Enter to continue");
+            Console.ReadLine();
+
+            while (low != high)
+            {
+                int mid = (low + high) / 2;
+                Console.WriteLine($"Press 1 if your number is in between {low} and {mid} \nPress 2 if your number is in between {mid + 1} and {high}");
+                int choose = Convert.ToInt32(Console.ReadLine());
+                count++;
+                switch (choose)
+                {
+                    case 1:
+                        high = mid;
+                        break;
+                    case 2:
+                        low = mid + 1;
+                        break;
+                }
+            }
+            Console.WriteLine($"Your Number is {low} !!!!!");
+            Console.WriteLine($"Found your number in {count} tries");
+            FindNumber();
+        }
+    }
+}
--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -6,64 +6,9 @@
         {
             Console.WriteLine("Welcome to Algorithm Programs");
 
-            Console.WriteLine("Select Search and Sort Method from Below: ");
-            Console.WriteLine("1. Binary Search \n2. Insertion Sort \n3. Bubble Sort \n4. Merge Sort");
-            int option = Convert.ToInt32(Console.ReadLine());
-
-            switch (option)
-            {
-                case 1:
-
-                    BinarySearchWord<string> obj = new BinarySearchWord<string>();
-                    obj.WordExistsOrNot<string>();
-                    Console.ReadLine();
-                    break;
-
-                case 2:
-
-                    string[] names = { "OOPS", "Array", "Dictionary", "Generics", "Delegates", "LinkedList", "Stack", "Queue", "HashTable", "Events", "Regular Expressions" };
-                    InsertionSortMethod<string> objInsertion = new InsertionSortMethod<string>();
-
-                    Console.WriteLine("\nBefore sorting array elements are: ");
-                    objInsertion.Display<string>(names);
-
-                    objInsertion.Sort<string>(names);
-                    Console.WriteLine("\n\nAfter sorting array elements are: ");
-                    objInsertion.Display<string>(names);
-                    Console.ReadLine();
-                    break;
-
-                case 3:
-
-                    int[] array = { 51, 12, 49, 26, 85, 153, 4 };
-                    BubbleSortMethod<int> objBubbleSort = new BubbleSortMethod<int>();
-
-                    Console.WriteLine("\nBefore sorting array elements are: ");
-                    objBubbleSort.Display<int>(array);
-
-                    objBubbleSort.Sort<int>(array);
-                    Console.WriteLine("\n\nAfter sorting array elements are: ");
-                    objBubbleSort.Display<int>(array);
-
-                    Console.ReadLine();
-                    break;
-
-                case 4:
-
-                    int[] numArray = { 51, 12, 49, 26, 85, 153, 4 };
-                    MergeSortMethod<int> objSort = new MergeSortMethod<int>();
-
-                    Console.WriteLine("\nBefore sorting array elements are: ");
-                    objSort.Display<int>(numArray);
-
-                    objSort.Sort<int>(numArray, 0, numArray.Length - 1);
-                    Console.WriteLine("\n\nAfter sorting array elements are: ");
-                    objSort.Display<int>(numArray);
-
-                    Console.ReadLine();
-                    break;
-            }
-
+            FindYourNumber obj= new FindYourNumber();
+            obj.FindNumber();
+            Console.ReadLine();
         }
     }
 }
